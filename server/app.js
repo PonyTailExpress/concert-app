92fffb57e20bc59ac0b2a5c038afd77201e15fa9
hamid89 <hamid.khurshid24@gmail.com>
2025-03-05 11:18:28 +0100

    update on branch develop

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config(); // To load environment variables
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(morgan("dev"))
 app.use(express.json()); // For parsing JSON bodies
 
 // Connect to MongoDB
---
+++ server/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  concertLikes: 
+    [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Concert" }
+    ],
+  
+  artistLikes: 
+    [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Artist" }
+    ]
+  
+});
+
+module.exports = model("User", userSchema);
